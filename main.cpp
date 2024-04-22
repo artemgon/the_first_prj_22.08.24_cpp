5bc01f40a79995494c1221426a39cfc72220a248
artemgon <artemhoncharuk123@gmail.com>
2024-04-22 19:57:34 +0300

    Creating the third task of the practice work.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+	cout << "Calculating the volume of parallelepiped.\n";
+	cout << "Please, enter the following data:\n";
+	float length;
+	float width;
+	float height;
+	cout << "Length: ";
+	cin >> length;
+	cout << "Width: ";
+	cin >> width;
+	cout << "Height: ";
+	cin >> height;
+	float volume = length * width * height;
+	cout << "Parallelepiped's volume = " << volume;
 	return 0;
 }
