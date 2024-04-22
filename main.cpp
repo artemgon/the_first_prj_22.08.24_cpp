98199add8bd3e902562bf9e0edcbd616b8f35e1f
artemgon <artemhoncharuk123@gmail.com>
2024-04-22 20:09:02 +0300

    Creating the fourth task of practice work.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+	cout << "Calculating the length between two towns" << "\nPlease, enter the following data;\n";
+	int map_scale;
+	cout << "Map scale: ";
+	cin >> map_scale;
+	float length;
+	cout << "The length between two towns on your map (cm): ";
+	cin >> length;
+	cout << "The length between two towns is: " << map_scale * length;
 	return 0;
 }
