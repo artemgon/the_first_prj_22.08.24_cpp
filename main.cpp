a38bf8fa0a15772527794af0773f280d2f6986b3
artemgon <artemhoncharuk123@gmail.com>
2024-04-22 20:26:11 +0300

    Creating the fifth task of the practice work.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
+#define _USE_MATH_DEFINES 
 #include <iostream>
-#include <math.h>
+#include <cmath>
 using namespace std;
 
 int main() {
 	float sphere_radius;
 	cout << "Please, enter the sphere radius:";
 	cin >> sphere_radius;
-	float sphere_volume = 4 / 3 * sphere_radius;
+	float sphere_volume = 4 / 3 * M_PI * sphere_radius;
+	cout << "Sphere_volume is: " << sphere_volume;
 	return 0;
 }
