127ce390886a01004f0779cee79e874835cc9740
artemgon <artemhoncharuk123@gmail.com>
2024-04-22 19:44:44 +0300

    Creating the first part of the second task of the pw.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
+	int coins;
+	cout << "Please, enter the quantity of coins: ";
+	cin >> coins;
+	int banknotes;
+	cout << "Please, enter the quantity of banknotes: ";
+	cin >> banknotes;
 	return 0;
 }
