77879073084f7241192537eeb21f27bb4c57f366
artemgon <artemhoncharuk123@gmail.com>
2024-04-22 20:19:06 +0300

    Creating the fifth task.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main() {
+	float sphere_radius;
+	cout << "Please, enter the sphere radius:";
+	cin >> sphere_radius;
+	float sphere_volume = 4 / 3 * sphere_radius;
 	return 0;
 }
