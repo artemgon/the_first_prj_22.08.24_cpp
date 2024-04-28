7663b7f657c70456e76c144707fea7a92c1631d8
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 20:15:42 +0300

    Creating the third task of homework.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main() {
-	float fraction;
-	cout << "Please, enter some fraction(float type):";
-	cin >> fraction;
-	int hryven = int(fraction);
-	cout << "It's " << hryven << " hryven and " << (fraction - hryven) * 100 << " kopiyok";
+	cout << "Calculating the run velocity";
+	int length;
+	cout << "\nPlease, enter the distance length(m):";
+	cin >> length;
+	float time;
+	cout << "Please, enter the time(min.s):";
+	cin >> time;
+	cout << "Distance: " << length;
+	float seconds = int(time) * 60 + (time - int(time)) * 100;
+	cout << "\nTime: " << seconds;
+	cout << "\nThe velocity is: " << (length/seconds) * 3.6 << " km/h";
 	return 0;
 }
