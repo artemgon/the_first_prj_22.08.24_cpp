53f869f819e38e82c5675ef5696af1217975cf5f
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 18:46:21 +0300

    Creating the first task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
+	int seconds;
+	cout << "Please, enter the time in seconds:";
+	cin >> seconds;
+	cout << "It's " << seconds / 3600 << " hours\n";
+	cout << "It's " << seconds / 60 << " minutes\n";
+	cout << "It's " << seconds << " seconds\n";
 	return 0;
 }
