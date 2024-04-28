773f96fc1947273c0f3bac9bd2d2ba9401b90e80
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 18:55:20 +0300

    Creating the second task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
+	float fraction;
+	cout << "Please, enter some fraction(float type):";
+	cin >> fraction;
+	int hryven = int(fraction);
+	cout << "It's " << hryven << " hryven and " << (fraction - hryven) * 100 << " kopiyok";
 	return 0;
 }
