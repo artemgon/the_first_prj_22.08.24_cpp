23027274ffa380a3ff90edb36f60dff4cf56241f
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 20:22:21 +0300

    Creating the fourth task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
+	int days;
+	cout << "Please, type the quantity of days: ";
+	cin >> days;
+	int days_2 = days % 7;
+	int weeks = (days - days_2) / 7;
+	cout << "The result is: " << weeks << " weeks and " << days_2 << " days";
 	return 0;
 }
