4cc1a3c421e6c214ae64cd37db97208df7c10de7
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 17:25:36 +0300

    Creating the first task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main() {
 	int year;
 	cout << "Please, enter the year: ";
 	cin >> year;
+	bool condition = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+	int days = 365 + condition;
+	cout << "In " << year << " year = " << days << " days";
 	return 0;
 }
