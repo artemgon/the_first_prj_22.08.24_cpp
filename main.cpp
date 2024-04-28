8a50ca66d37ca7889f42df56d4cc8fa90e5a992b
artemgon <artemhoncharuk123@gmail.com>
2024-04-28 18:21:23 +0300

    Creating the second task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main() {
-	int coins;
-	cout << "Please, enter the quantity of coins: ";
-	cin >> coins;
 	int banknotes;
 	cout << "Please, enter the quantity of banknotes: ";
 	cin >> banknotes;
+	int coins;
+	cout << "Please, enter the quantity of coins: ";
+	cin >> coins;
+	banknotes += (coins / 100);
+	coins = (coins / 10 % 10) * 10 + coins % 10;
+	cout << banknotes << " hryven" << " " << coins << " kopiyok ";
 	return 0;
 }
